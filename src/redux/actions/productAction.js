4f00b0c717d361eeaafda3d849a97144de575854
KimHeeSoo <khstoday@naver.com>
2024-08-27 16:01:06 +0900

    commit

--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -2,7 +2,7 @@ import { productActions } from "../reducers/productReducer";
 
 function getProducts(searchQuery, typeQuery, menuQuery) {
   return async (dispatch) => {
-    let url = `http://localhost:5000/products`;
+    let url = `https://my-json-server.typicode.com/KimHeeH/zara_mall/products`;
     let params = new URLSearchParams();
 
     if (searchQuery) params.append("q", searchQuery);
@@ -18,7 +18,7 @@ function getProducts(searchQuery, typeQuery, menuQuery) {
 
 function getProductDetail(id) {
   return async (dispatch) => {
-    let url = `http://localhost:5000/products/${id}`;
+    let url = `https://my-json-server.typicode.com/KimHeeH/zara_mall/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     console.log("data는", data);
